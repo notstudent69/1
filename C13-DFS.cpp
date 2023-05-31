e2e7e1879e8513b03499abded3bff9e36d1b9e4c
notStudent69 <134788809+notstudent69@users.noreply.github.com>
2023-05-31 21:47:42 +0530

    Update C13-DFS.cpp

--- C13-DFS.cpp
+++ C13-DFS.cpp
@@ -1,76 +1,55 @@
 #include <iostream>
-#include <vector>
 #include <map>
-#include <stack>
+#include <queue>
+#include <set>
+#include <limits>
 
 using namespace std;
 
-// Function to perform Depth First Search (DFS)
-void DFS(vector<vector<int> >& graph, map<int, string>& landmarks, int startNode) {
-    int numNodes = graph.size();
+// Function to perform Depth-First Search (DFS)
+void DFS(map<int, set<int> >& graph, map<int, bool>& visited, int node) {
+    cout << node << " "; // Print the current node
+    visited[node] = true; // Mark the current node as visited
 
-    vector<bool> visited(numNodes, false);
-    stack<int> dfsStack;
-
-    dfsStack.push(startNode);
-    visited[startNode] = true;
-
-    cout << "DFS traversal starting from " << landmarks[startNode] << ":" << endl;
-
-    while (!dfsStack.empty()) {
-        int currentNode = dfsStack.top();
-        dfsStack.pop();
-
-        cout << landmarks[currentNode] << " ";
-
-        for (int neighbor = 0; neighbor < numNodes; ++neighbor) {
-            if (graph[currentNode][neighbor] == 1 && !visited[neighbor]) {
-                dfsStack.push(neighbor);
-                visited[neighbor] = true;
-            }
+    // Iterate through all the adjacent nodes
+    for (set<int>::iterator it = graph[node].begin(); it != graph[node].end(); ++it) {
+        int adjacentNode = *it;
+        // If the adjacent node is not visited, recursively call DFS on it
+        if (!visited[adjacentNode]) {
+            DFS(graph, visited, adjacentNode);
         }
     }
-
-    cout << endl;
 }
 
 int main() {
-    // Create the graph using an adjacency matrix
-    vector<vector<int> > graph;
-    graph.resize(6, vector<int>(6, 0));
-    graph[0][1] = 1;
-    graph[0][4] = 1;
-    graph[1][0] = 1;
-    graph[1][2] = 1;
-    graph[1][3] = 1;
-    graph[1][4] = 1;
-    graph[2][1] = 1;
-    graph[2][3] = 1;
-    graph[2][5] = 1;
-    graph[3][1] = 1;
-    graph[3][2] = 1;
-    graph[3][4] = 1;
-    graph[4][0] = 1;
-    graph[4][1] = 1;
-    graph[4][3] = 1;
-    graph[4][5] = 1;
-    graph[5][2] = 1;
-    graph[5][4] = 1;
+    // Create a map to represent the graph
+    map<int, set<int> > graph;
 
-    
-    map<int, string> landmarks;
-    landmarks[0] = "Park";
-    landmarks[1] = "Library";
-    landmarks[2] = "Cafeteria";
-    landmarks[3] = "Auditorium";
-    landmarks[4] = "Sports_Complex";
-    landmarks[5] = "Hostel";
-
-    int startNode = 0; 
-
-    
-    DFS(graph, landmarks, startNode);
-
-    return 0;
+    // Take input for the number of nodes
+    int numNodes;
+    cout << "Enter the number of nodes: ";
+    while (!(cin >> numNodes) || numNodes <= 0) {
+        cout << "Invalid input. Please enter a positive integer: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    // Take input for the connections between landmarks
+    int landmark1, landmark2;
+    cout << "Enter the connections between landmarks (enter -1 -1 to stop):\n";
+    while (cin >> landmark1 >> landmark2 && (landmark1 != -1 || landmark2 != -1)) {
+        graph[landmark1].insert(landmark2);
+        graph[landmark2].insert(landmark1);
+    }
+    // Perform Depth-First Search (DFS) using the adjacency list
+    map<int, bool> visitedDFS;
+    int startNode;
+    cout << "Enter the starting node for DFS: ";
+    while (!(cin >> startNode) || graph.find(startNode) == graph.end()) {
+        cout << "Invalid node. Please enter a valid node: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "DFS traversal: ";
+    DFS(graph, visitedDFS, startNode);
+    cout << endl;
 }
-
