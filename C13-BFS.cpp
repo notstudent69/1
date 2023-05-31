9b1d9a57abea814842b992432735096ad7ce601c
notStudent69 <134788809+notstudent69@users.noreply.github.com>
2023-05-31 21:47:14 +0530

    Update C13-BFS.cpp

--- C13-BFS.cpp
+++ C13-BFS.cpp
@@ -1,82 +1,67 @@
 #include <iostream>
-#include <vector>
 #include <map>
 #include <queue>
+#include <set>
+#include <limits>
 
 using namespace std;
 
-// Function to perform Breadth First Search (BFS)
-void BFS(vector<vector<int> >& graph, map<int, string>& landmarks, int startNode) {
-    int numNodes = graph.size();
-
-    vector<bool> visited(numNodes, false);
-    queue<int> bfsQueue;
-
-    bfsQueue.push(startNode);
+void BFS(map<int, set<int> >& graph, map<int, bool>& visited, int startNode) {
+    queue<int> q;
+    q.push(startNode);
     visited[startNode] = true;
 
-    cout << "BFS traversal starting from " << landmarks[startNode] << ":" << endl;
+    while (!q.empty()) {
+        int currentNode = q.front();
+        q.pop();
+        cout << currentNode << " ";
 
-    while (!bfsQueue.empty()) {
-        int currentNode = bfsQueue.front();
-        bfsQueue.pop();
-
-        cout << landmarks[currentNode] << " ";
-
-        for (int i = 0; i < graph[currentNode].size(); ++i) {
-            int neighbor = graph[currentNode][i];
-            if (!visited[neighbor]) {
-                bfsQueue.push(neighbor);
-                visited[neighbor] = true;
+        // Iterate through all the adjacent nodes
+        for (set<int>::iterator it = graph[currentNode].begin(); it != graph[currentNode].end(); ++it) {
+            int adjacentNode = *it;
+            // If the adjacent node is not visited, mark it as visited and enqueue it
+            if (!visited[adjacentNode]) {
+                visited[adjacentNode] = true;
+                q.push(adjacentNode);
             }
         }
     }
-
-    cout << endl;
 }
 
 int main() {
-    // Create the graph using an adjacency list
-    vector<vector<int> > graph(6);
+    // Create a map to represent the graph
+    map<int, set<int> > graph;
 
-    graph[0].push_back(1);
-    graph[0].push_back(4);
+    // Take input for the number of nodes
+    int numNodes;
+    cout << "Enter the number of nodes: ";
+    while (!(cin >> numNodes) || numNodes <= 0) {
+        cout << "Invalid input. Please enter a positive integer: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    // Take input for the connections between landmarks
+    int landmark1, landmark2;
+    cout << "Enter the connections between landmarks (enter -1 -1 to stop):\n";
+    while (cin >> landmark1 >> landmark2 && (landmark1 != -1 || landmark2 != -1)) {
+        graph[landmark1].insert(landmark2);
+        graph[landmark2].insert(landmark1);
+    }
 
-    graph[1].push_back(0);
-    graph[1].push_back(2);
-    graph[1].push_back(3);
-    graph[1].push_back(4);
-
-    graph[2].push_back(1);
-    graph[2].push_back(3);
-    graph[2].push_back(5);
-
-    graph[3].push_back(1);
-    graph[3].push_back(2);
-    graph[3].push_back(4);
-
-    graph[4].push_back(0);
-    graph[4].push_back(1);
-    graph[4].push_back(3);
-    graph[4].push_back(5);
-
-    graph[5].push_back(2);
-    graph[5].push_back(4);
-
-    // Create a map of landmarks
-    map<int, string> landmarks;
-    landmarks[0] = "Park";
-    landmarks[1] = "Library";
-    landmarks[2] = "Cafeteria";
-    landmarks[3] = "Auditorium";
-    landmarks[4] = "Sports_Complex";
-    landmarks[5] = "Hostel";
-
-    int startNode = 0; // Starting node for BFS
-
-    // Perform BFS on the graph
-    BFS(graph, landmarks, startNode);
+    // Perform Breadth-First Search (BFS) using the adjacency list
+    int startNode;
+    map<int, bool> visitedBFS;
+    cout << "Enter the starting node for BFS: ";
+    while (!(cin >> startNode) || graph.find(startNode) == graph.end()) {
+        cout << "Invalid node. Please enter a valid node: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "BFS traversal: ";
+    BFS(graph, visitedBFS, startNode);
+    cout << endl;
 
     return 0;
 }
+
 
